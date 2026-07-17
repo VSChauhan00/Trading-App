b421d61c02943660115521bd5405ca42f72cb98d
Vivek Singh Chauhan <vschauhan236679@gmail.com>
2026-07-17 22:27:57 +0530

    Updated Stats, Pricing and Education section in home page

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -2,7 +2,42 @@ import React from 'react';
 
 function Pricing() {
     return ( 
-        <h1>Pricing</h1>
+        <div className='container mb-5'>
+            <div className='row'>
+                <div className='col-4'>
+                    <h1 className='mb-3'>Unbeatable pricing</h1>
+                    <p className='text-muted mb-2'>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                    <a href='' style={{textDecoration:'none'}}>
+                        See pricing
+                        <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                    </a>
+                </div>
+                <div className='col-2'></div>
+                <div className='col-6'>
+                    <div className='row'>
+                        <div className='col-2'>
+                            <img src='media/images/pricing0.svg'/>
+                        </div>
+                        <div className='col-2'>
+                            <p>Free account <br/>opening</p>
+                        </div>
+                        <div className='col-2'>
+                            <img src='media/images/pricing0.svg'/>
+                        </div>
+                        <div className='col-2'>
+                            <p>Free equity delivery <br/>and direct mutual funds</p>
+                        </div>
+                        <div className='col-2'>
+                            <img src='media/images/intradayTrades.svg'/>
+                        </div>
+                        <div className='col-2'>
+                            <p>Intraday and F&O</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
 
