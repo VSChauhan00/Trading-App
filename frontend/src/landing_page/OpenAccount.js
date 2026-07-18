782faf69ce5245e7e8ed7cd726dec6bcfde18102
Vivek Singh Chauhan <vschauhan236679@gmail.com>
2026-07-19 00:05:16 +0530

    Created Navbar and Footer and updated Open account section for home page

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 function OpenAccount() {
     return ( 
-        <h1>OpenAccount</h1>
+        <div className='container p-5'>
+            <div className='row text-center'>
+                <h1 className='m-5'>Open a Zerodha account</h1>
+                <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+                <button type="button" class="p-2 btn btn-primary fs-5 mt-2" style={{width:"20%", margin: "0 auto"}}>Signup for free</button>
+            </div>
+        </div>
      );
 }
 
