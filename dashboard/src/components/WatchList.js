7a66fe8cd15b9a8ac6d9fe05b2ca97f7c9668e15
Vivek Singh Chauhan <vschauhan236679@gmail.com>
2026-08-02 18:07:12 +0530

    Created watchlist section and added values of data stored

--- dashboard/src/components/WatchList.js
+++ dashboard/src/components/WatchList.js
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Tooltip, Grow } from '@mui/material';
+
+import {KeyboardArrowDown, KeyboardArrowUp} from '@mui/icons-material';
+
+import { watchlist } from "../data/data";
 
 const WatchList = () => {
   return (
@@ -11,12 +17,46 @@ const WatchList = () => {
           placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
           className="search"
         />
-        <span className="counts"> 9 / 50</span>
+        <span className="counts"> {watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return (
+          <WatchListItem stock={stock} key={index}/>
+          );
+        })}
+      </ul>
     </div>
   );
 };
 
 export default WatchList;
+
+const WatchListItem = ({stock}) => {
+  const [showWhatchListAction, setShowWatchListActions] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setShowWatchListActions(true);
+  }
+
+  const handleMouseExit = (e) => {
+    setShowWatchListActions(false);
+  }
+
+  return (
+    <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseExit}>
+      <div className="item">
+        <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+        <div className="itemInfo">
+          <span className="percent">{stock.percent}</span>
+          {stock.isDown ? (
+            <KeyboardArrowDown className='down' />
+          ) : <KeyboardArrowUp className='up' />}
+
+          <span className="price">{stock.price.toFixed(2)}</span>
+        </div>
+      </div>
+    </li>
+  )
+}
